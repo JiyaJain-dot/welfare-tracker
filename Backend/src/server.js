4cf20049298d88d80bcca72d5c8a378b15f8652c
ShaunT06 <shaunthomas116@gmail.com>
2026-08-15 23:49:21 +0530

    Added URL to CORS

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://welfare-tracker-mauve.vercel.app'
   ]
 }));
 
